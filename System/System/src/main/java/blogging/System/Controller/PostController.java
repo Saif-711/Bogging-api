7489ce660a7203cf5f921564cac6b349339e9dcb
Saif-711 <saifnairat23@gmail.com>
2026-03-22 17:25:00 +0000

    create&implement Exception pkg ,Service pkg

---
+++ System/System/src/main/java/blogging/System/Controller/PostController.java
@@ -0,0 +1,13 @@
+package blogging.System.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/blog")
+public class PostController {
+
+
+}
---
+++ System/System/src/main/java/blogging/System/Service/PostService.java
@@ -0,0 +1,44 @@
+package blogging.System.Service;
+
+import blogging.System.Exception.ResourceNotFoundException;
+import blogging.System.Model.BlogPost;
+import blogging.System.Repo.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PostService {
+    @Autowired
+    private PostRepo postRepo;
+
+    public BlogPost createPost(BlogPost post) {
+       return postRepo.save(post);
+    }
+    public List<BlogPost> getAllPosts() {
+        return postRepo.findAll();
+    }
+    public BlogPost getPostById(Long id){
+         Optional<BlogPost>post=postRepo.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Post With id "+id+"Not found");
+                return post.get();
+
+    }
+
+    public void updatePost(Long id, BlogPost updatedPost) {
+        BlogPost post = getPostById(id);
+        post.setTitle(updatedPost.getTitle());
+        post.setContent(updatedPost.getContent());
+        post.setCategory(updatedPost.getCategory());
+        post.setTags(updatedPost.getTags());
+        postRepo.save(post);
+    }
+    public void deletePost(Long id) {
+        BlogPost post = getPostById(id);
+        postRepo.delete(post);
+    }
+
+}
